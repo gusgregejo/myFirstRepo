abb2c86eea5c9b47f5c9ec4c75cc11218f4120a4
gusgregejo <gusgregejo@student.gu.se>
2016-01-18 15:46:25 +0100

    i changed something, i added a new printout and changed the old one

--- hello.java
+++ hello.java
@@ -1,7 +1,8 @@
 public class hello{
 
 public static void main(String[] args){
-System.out.print("hello");
+System.out.print("Hello world");
+System.out.print("Goodbuy! that was fast wopwop!");
 
 }
 
